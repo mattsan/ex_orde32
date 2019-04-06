fbd28b8cb1b81ccbb959659ceca3490a00fd937e
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-04-06 17:06:48 +0900

    map rects

--- lib/ex_orde32.ex
+++ lib/ex_orde32.ex
@@ -4,6 +4,35 @@ defmodule ExOrde32 do
   """
 
   def solve(input) do
-    input
+    source_rects =
+      input
+      |> String.split("/", trim: true)
+      |> Enum.map(&string_rect/1)
+
+    source_count = Enum.count(source_rects)
+
+
+    Enum.reduce(0..35, %{}, fn x, cells ->
+      Enum.reduce(0..35, cells, fn y, cells2 ->
+        value =
+          (0..source_count-1)
+          |> Enum.filter(fn i ->
+            rect = Enum.at(source_rects, i)
+            rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom
+          end)
+        Map.put(cells2, {x, y}, value)
+      end)
+    end)
+
+    inspect source_rects
+  end
+
+  defp string_rect(s) do
+    [left, top, right, bottom] =
+      s
+      |> String.split("", trim: true)
+      |> Enum.map(&String.to_integer(&1, 36))
+
+    %{left: left, top: top, right: right, bottom: bottom}
   end
 end
