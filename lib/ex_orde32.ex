11ee19f81b55762c911c49286dff8caa4fc381e7
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-04-06 17:32:49 +0900

    the first solution

--- lib/ex_orde32.ex
+++ lib/ex_orde32.ex
@@ -11,20 +11,27 @@ defmodule ExOrde32 do
 
     source_count = Enum.count(source_rects)
 
-
-    Enum.reduce(0..35, %{}, fn x, cells ->
-      Enum.reduce(0..35, cells, fn y, cells2 ->
-        value =
-          (0..source_count-1)
-          |> Enum.filter(fn i ->
-            rect = Enum.at(source_rects, i)
-            rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom
-          end)
-        Map.put(cells2, {x, y}, value)
+    result_cells =
+      Enum.reduce(0..35, %{}, fn x, cells ->
+        Enum.reduce(0..35, cells, fn y, cells2 ->
+          value =
+            (0..source_count-1)
+            |> Enum.filter(fn i ->
+              rect = Enum.at(source_rects, i)
+              rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom
+            end)
+          Map.put(cells2, {x, y}, value)
+        end)
       end)
-    end)
 
-    inspect source_rects
+    result_cells
+    |> Map.values()
+    |> Enum.uniq()
+    |> Enum.filter(&Enum.count(&1) > 0)
+    |> Enum.map(&calc_rect_size(result_cells, &1))
+    |> Enum.filter(&(&1 > 0))
+    |> Enum.sort()
+    |> Enum.join(",")
   end
 
   defp string_rect(s) do
@@ -34,5 +41,20 @@ defmodule ExOrde32 do
       |> Enum.map(&String.to_integer(&1, 36))
 
     %{left: left, top: top, right: right, bottom: bottom}
+  end
+
+  defp calc_rect_size(cells, value) do
+    target_cells = for x <- (0..35), y <- (0..35), Map.get(cells, {x, y}) == value, do: {x, y}
+
+    target_cell_count = Enum.count(target_cells)
+
+    {left, right} = target_cells |> Enum.map(&elem(&1, 0)) |> Enum.min_max()
+    {top, bottom} = target_cells |> Enum.map(&elem(&1, 1)) |> Enum.min_max()
+
+    if (right - left + 1) * (bottom - top + 1) == target_cell_count do
+      target_cell_count
+    else
+      0
+    end
   end
 end
