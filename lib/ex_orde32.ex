10e09efdd772d199e277708eb25ff1f05ad0e111
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-04-06 15:24:28 +0900

    initial commit

---
+++ lib/ex_orde32.ex
@@ -0,0 +1,18 @@
+defmodule ExOrde32 do
+  @moduledoc """
+  Documentation for ExOrde32.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExOrde32.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_orde32_test.exs
@@ -0,0 +1,8 @@
+defmodule ExOrde32Test do
+  use ExUnit.Case
+  doctest ExOrde32
+
+  test "greets the world" do
+    assert ExOrde32.hello() == :world
+  end
+end
